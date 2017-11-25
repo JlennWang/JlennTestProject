6b2e1c9d68e16173318c6836ca829a3c9268eb4a
Jlenn <Jlenn@JlenndeMac-mini.local>
2017-11-25 23:40:15 +0800

    Jlenn第一次提交代码

---
+++ GItHubDemo1/GItHubDemo1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GItHubDemo1
+//
+//  Created by Jlenn on 2017/11/25.
+//  Copyright © 2017年 Jlenn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GItHubDemo1/GItHubDemo1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GItHubDemo1
+//
+//  Created by Jlenn on 2017/11/25.
+//  Copyright © 2017年 Jlenn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
